c295d15640afb5c34172b166de5b260ed39b8fc0
Srikanth  Srungarapu <ssrungar@maymakeloft-lm.champ.corp.yahoo.com>
2013-08-15 23:57:06 -0500

    Server changes

--- gen/com/example/yahoodealsapp/R.java
+++ gen/com/example/yahoodealsapp/R.java
@@ -32,21 +32,20 @@ public final class R {
     public static final class drawable {
         public static final int arrow=0x7f020000;
         public static final int barcode=0x7f020001;
-        public static final int barcodeblackstripes=0x7f020002;
-        public static final int gradient_bg=0x7f020003;
-        public static final int gradient_bg_hover=0x7f020004;
-        public static final int heart=0x7f020005;
-        public static final int ic_action_search=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int icon=0x7f020008;
-        public static final int image_bg=0x7f020009;
-        public static final int list_selector=0x7f02000a;
-        public static final int no_image=0x7f02000b;
-        public static final int play=0x7f02000c;
-        public static final int rihanna=0x7f02000d;
-        public static final int yahoo_app=0x7f02000e;
-        public static final int yahoo_deals=0x7f02000f;
-        public static final int yahoo_deals1=0x7f020010;
+        public static final int gradient_bg=0x7f020002;
+        public static final int gradient_bg_hover=0x7f020003;
+        public static final int heart=0x7f020004;
+        public static final int ic_action_search=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int icon=0x7f020007;
+        public static final int image_bg=0x7f020008;
+        public static final int list_selector=0x7f020009;
+        public static final int no_image=0x7f02000a;
+        public static final int play=0x7f02000b;
+        public static final int rihanna=0x7f02000c;
+        public static final int yahoo_app=0x7f02000d;
+        public static final int yahoo_deals=0x7f02000e;
+        public static final int yahoo_deals1=0x7f02000f;
     }
     public static final class id {
         public static final int action_search=0x7f0b0017;
--- src/com/example/yahoodealsapp/SelectedDeal.java
+++ src/com/example/yahoodealsapp/SelectedDeal.java
@@ -1,9 +1,5 @@
 package com.example.yahoodealsapp;
 
-
-
-
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,101 +33,89 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.provider.Settings;
-
-
+import android.provider.Settings.Secure;
 
 public class SelectedDeal extends Activity {
 	// XML node keys
-		static final String KEY_SONG = "song"; // parent node
-		static final String KEY_ID = "id";
-		static final String KEY_TITLE = "title";
-		static final String KEY_ARTIST = "artist";
-		static final String KEY_DURATION = "duration";
-		static final String KEY_THUMB_URL = "thumb_url";
-		
-		
-		/*DEALS GLOBAL VALUES */
-		static final String DEAL_ROWS = "DEALS";
-		static final String DEAL_NAME = "DEALNAME";
-		static final String DEAL_COMPANY_NAME = "COMPANYNAME";
-		static final String DEAL_RATING = "RATING";
-		static final String DEAL_ADDRESS = "ADDRESS";
-		static final String DEAL_LATITUDE = "LAT";
-		static final String DEAL_LONGITUDE = "LONG";
-		static final String DEAL_IMG_URL = "IMAGE_URL";
-		static final String DISTANCE_TO_DEAL = "DISTANCE";
-		static final String DEAL_CITY = "CITY";
-		static final String DEAL_STATE = "STATE";
-		static final String DEAL_PHONE = "PHONE";
-		private static final float THRESHOLD = 0;
-		
+	static final String KEY_SONG = "song"; // parent node
+	static final String KEY_ID = "id";
+	static final String KEY_TITLE = "title";
+	static final String KEY_ARTIST = "artist";
+	static final String KEY_DURATION = "duration";
+	static final String KEY_THUMB_URL = "thumb_url";
 
-		/*END OF DEALS GLOBAL VALUES*/
-		
-		
+	/* DEALS GLOBAL VALUES */
+	static final String DEAL_ROWS = "DEALS";
+	static final String DEAL_NAME = "DEALNAME";
+	static final String DEAL_COMPANY_NAME = "COMPANYNAME";
+	static final String DEAL_RATING = "RATING";
+	static final String DEAL_ADDRESS = "ADDRESS";
+	static final String DEAL_LATITUDE = "LAT";
+	static final String DEAL_LONGITUDE = "LONG";
+	static final String DEAL_IMG_URL = "IMAGE_URL";
+	static final String DISTANCE_TO_DEAL = "DISTANCE";
+	static final String DEAL_CITY = "CITY";
+	static final String DEAL_STATE = "STATE";
+	static final String DEAL_PHONE = "PHONE";
+	private static final float THRESHOLD = 0;
+
+	/* END OF DEALS GLOBAL VALUES */
+
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        setContentView(R.layout.selected_deal);
-        
-        // getting intent data
-        Intent in = getIntent();
-        
-        // Get XML values from previous intent
-        String name = in.getStringExtra(DEAL_NAME);
-        String companyName = in.getStringExtra(DEAL_COMPANY_NAME);
-        String description = in.getStringExtra(DEAL_RATING);
-        String url = in.getStringExtra(DEAL_IMG_URL);
-        String latitude = in.getStringExtra(DEAL_LATITUDE);
-        String longitude = in.getStringExtra(DEAL_LONGITUDE);
-        String Address = in.getStringExtra(DEAL_ADDRESS);
-        String City = in.getStringExtra(DEAL_CITY);
-        String State = in.getStringExtra(DEAL_STATE);
-        String Phone = in.getStringExtra(DEAL_PHONE);
-        String distance = in.getStringExtra(DISTANCE_TO_DEAL);
-        
-        String AddressWhole = "Contact:\n"+Address+"\n"+City+"\n"+State +"\n" +Phone;
-        
-        
-        TextView dealCompany = (TextView) findViewById(R.id.dealCompany);
-        TextView dealName = (TextView) findViewById(R.id.dealName);
-        TextView dealDistance = (TextView) findViewById(R.id.dealDistance);
-        TextView dealAddress = (TextView) findViewById(R.id.dealAddress);
-        ImageView dealBarCode = (ImageView) findViewById(R.id.dealBarCode);
-        
-        // Displaying all values on the screen
-        /*
-        
-        TextView dealRating = (TextView) findViewById(R.id.dealRating);
-       
-        
-      
-        */
-    
-        dealCompany.setText(companyName);
-        dealName.setText(name);
-        dealDistance.setText("Distance to Deal: " +distance );
-        dealAddress.setText(AddressWhole);
-        dealBarCode.setAlpha(127);
-        //Bitmap bitmap = (Bitmap)this.getIntent().getParcelableExtra("selectedImage");  
-        ImageView image = (ImageView)findViewById(R.id.list_image_selected);
-        //image.setImageBitmap(bitmap);
-        /*
-        
-        dealRating.setText("Rating: "+description);
-        
-        lblDistance
-		.setText(String.valueOf(determineEvent(latitude, longitude)));
-         
-        Bitmap bitmap=ImageLoader.memoryCache.get(url);
-        if(bitmap!=null)
-            image.setImageBitmap(bitmap);
-        
-         */
-        
-        
-        addActionListener(latitude, longitude);
+		setContentView(R.layout.selected_deal);
+
+		// getting intent data
+		Intent in = getIntent();
+
+		// Get XML values from previous intent
+		String name = in.getStringExtra(DEAL_NAME);
+		String companyName = in.getStringExtra(DEAL_COMPANY_NAME);
+		String description = in.getStringExtra(DEAL_RATING);
+		String url = in.getStringExtra(DEAL_IMG_URL);
+		String latitude = in.getStringExtra(DEAL_LATITUDE);
+		String longitude = in.getStringExtra(DEAL_LONGITUDE);
+		String Address = in.getStringExtra(DEAL_ADDRESS);
+		String City = in.getStringExtra(DEAL_CITY);
+		String State = in.getStringExtra(DEAL_STATE);
+		String Phone = in.getStringExtra(DEAL_PHONE);
+		String distance = in.getStringExtra(DISTANCE_TO_DEAL);
+
+		String AddressWhole = "Contact:\n" + Address + "\n" + City + "\n"
+				+ State + "\n" + Phone;
+
+		TextView dealCompany = (TextView) findViewById(R.id.dealCompany);
+		TextView dealName = (TextView) findViewById(R.id.dealName);
+		TextView dealDistance = (TextView) findViewById(R.id.dealDistance);
+		TextView dealAddress = (TextView) findViewById(R.id.dealAddress);
+		ImageView dealBarCode = (ImageView) findViewById(R.id.dealBarCode);
+
+		// Displaying all values on the screen
+		/*
+		 * 
+		 * TextView dealRating = (TextView) findViewById(R.id.dealRating);
+		 */
+
+		dealCompany.setText(companyName);
+		dealName.setText(name);
+		dealDistance.setText("Distance to Deal: " + distance);
+		dealAddress.setText(AddressWhole);
+		dealBarCode.setAlpha(127);
+		// Bitmap bitmap =
+		// (Bitmap)this.getIntent().getParcelableExtra("selectedImage");
+		ImageView image = (ImageView) findViewById(R.id.list_image_selected);
+		// image.setImageBitmap(bitmap);
+
+		// dealRating.setText("Rating: "+description);
+
+		Bitmap bitmap = ImageLoader.memoryCache.get(url);
+		if (bitmap != null)
+			image.setImageBitmap(bitmap);
+
+		addActionListener(latitude, longitude);
+		notifyServer(name, latitude, longitude);
 	}
 
 	class MyListener implements View.OnClickListener {
@@ -145,45 +129,41 @@ public class SelectedDeal extends Activity {
 
 		@Override
 		public void onClick(View v) {
-			String uri = "geo:"+ latitude + "," + longitude;
-	    	startActivity(new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri)));
+			String uri = "geo:" + latitude + "," + longitude;
+			startActivity(new Intent(android.content.Intent.ACTION_VIEW,
+					Uri.parse(uri)));
 		}
 
 	};
-	
+
 	private void addActionListener(String latitude, String longitude) {
-		/*Button button= (Button) findViewById(R.id.getDirections);
+		Button button = (Button) findViewById(R.id.getDirections);
 		button.setOnClickListener(new MyListener(latitude, longitude));
-		*/
 	}
+
 	public enum Event {
 		CLICK, CONVERSION, ACQUISTION
 	}
-	
-	private void notifyServer(String name, String cost, String latitude,
+
+	private void notifyServer(String name, String latitude,
 			String longitude) {
 		// TODO Auto-generated method stub
 		Event event = determineEvent(latitude, longitude);
 		HttpClient httpclient = new DefaultHttpClient();
-		HttpPost httppost = null;
-		switch (event) {
-		case CLICK:
-			httppost = new HttpPost("http://www.yoursite.com/script.php");
-			break;
-		case CONVERSION:
-			httppost = new HttpPost("http://www.yoursite.com/script.php");
-			break;
-		case ACQUISTION:
-			httppost = new HttpPost("http://www.yoursite.com/script.php");
-			break;
-		}
+		String android_id = Secure.getString(getBaseContext().getContentResolver(),
+                Secure.ANDROID_ID);
+		HttpPost httppost = new HttpPost(
+				"http://yodeals.herokuapp.com/hello?uid=" + android_id
+						+ "&deal=" + name.replaceAll("\\s","") + "&action=" + String.valueOf(event));
+		
 
 		try {
 			// Add your data
-			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-			nameValuePairs.add(new BasicNameValuePair("id", "12345"));
-			nameValuePairs.add(new BasicNameValuePair("stringdata", "Hi"));
-			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+//			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+//			nameValuePairs.add(new BasicNameValuePair("uid", android_id));
+//			nameValuePairs.add(new BasicNameValuePair("deal", name));
+//			nameValuePairs.add(new BasicNameValuePair("action", String.valueOf(event)));
+//			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
 			// Execute HTTP Post Request
 			HttpResponse response = httpclient.execute(httppost);
@@ -215,12 +195,12 @@ public class SelectedDeal extends Activity {
 				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 		float[] results = new float[1];
 		System.out.println(Double.valueOf(latitude));
-		
-		if(location != null) {
-		Location.distanceBetween(Double.valueOf(latitude),
-				Double.valueOf(longitude), location.getLatitude(),
-				location.getLongitude(), results);
-		Log.w("myapp", String.valueOf(results[0]));
+
+		if (location != null) {
+			Location.distanceBetween(Double.valueOf(latitude),
+					Double.valueOf(longitude), location.getLatitude(),
+					location.getLongitude(), results);
+			Log.w("myapp", String.valueOf(results[0]));
 		}
 		return results[0] > THRESHOLD ? Event.CLICK : Event.CONVERSION;
 	}
@@ -261,8 +241,8 @@ public class SelectedDeal extends Activity {
 
 	@Override
 	public void onBackPressed() {
-	  SelectedDeal.this.finish();
-	  overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
-	  //YahooDealsMain.stationsContainer.setVisibility(View.VISIBLE);
+		SelectedDeal.this.finish();
+		overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+		// YahooDealsMain.stationsContainer.setVisibility(View.VISIBLE);
 	}
 }
